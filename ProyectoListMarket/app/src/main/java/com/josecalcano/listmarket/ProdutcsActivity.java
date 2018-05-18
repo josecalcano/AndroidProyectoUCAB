cade6b3efc916fafed6bb5ea1261c0253b117215
josecalcano <jose.calcano07@gmail.com>
2018-05-18 18:12:07 -0400

    commit de inicio

---
+++ ProyectoListMarket/app/src/main/java/com/josecalcano/listmarket/ProdutcsActivity.java
@@ -0,0 +1,13 @@
+package com.josecalcano.listmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProdutcsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtcs);
+    }
+}
