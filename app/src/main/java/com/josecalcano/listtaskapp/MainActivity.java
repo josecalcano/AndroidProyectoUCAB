34a0bc712ca130b9944fe0402e0f1b65a1c3c83e
José Ángel Calcaño Azócar <jose.calcano07@gmail.com>
2018-07-01 13:53:13 -0400

    error de arranque en el manifest

--- app/src/main/java/com/josecalcano/listtaskapp/MainActivity.java
+++ app/src/main/java/com/josecalcano/listtaskapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements ListaTareas{
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        myToolbar.setTitle("ListTaskApp");
+        myToolbar.setTitle("listtaskapp");
         myToolbar.inflateMenu(R.menu.menu);
 
         config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
